af61b4236471b75f3b83d0aec836681f70a7a67d
marcinzak77 <marcin.zak77@gmail.com>
2018-09-07 14:00:53 +0200

    tasks fix

--- tasks_frontend/script.js
+++ tasks_frontend/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'http://protected-refuge-35061/v1/task/';
-  const trelloApiRoot = 'http://protected-refuge-35061/v1/trello/';
+  const apiRoot = 'http://protected-refuge-35061.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'http://protected-refuge-35061.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
